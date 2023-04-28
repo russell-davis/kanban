b693454826acb7f0d57f93eb981a55c7cc8671c1
Luke Davis <dev@rlukedavis.com>
2023-04-28 12:32:02 -0500

    move backlog to its own component

---
+++ src/components/backlog.tsx
@@ -0,0 +1,86 @@
+import { api, RouterOutputs } from "~/utils/api";
+import React, { useState } from "react";
+import { ActionIcon, Button, Text, TextInput } from "@mantine/core";
+import { startOfDay } from "date-fns";
+import { IconCheck } from "@tabler/icons-react";
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { Sortable } from "~/components/Sortable";
+import { TaskItem } from "~/components/TaskItem";
+
+export const Backlog = (props: {
+  goToTodayClicked: () => void;
+  tasksQueryData?: RouterOutputs["kanban"]["tasks"];
+}) => {
+  // newTitle state
+  const [newTitle, setNewTitle] = useState("");
+
+  // network
+  const utils = api.useContext();
+  const createTaskMutation = api.kanban.create.useMutation();
+
+  return (
+    <div className="BACKLOG flex min-w-[300px] flex-col bg-gray-800">
+      <div className="TITLE flex flex-row justify-between p-2">
+        <Text size={"lg"} weight={500} color={"white"}>
+          Backlog
+        </Text>
+        <Button compact onClick={props.goToTodayClicked}>
+          Today
+        </Button>
+      </div>
+      <div className="ACTIONS flex flex-row items-center justify-between space-x-2 p-2">
+        <TextInput
+          placeholder={"New task"}
+          classNames={{
+            root: "flex-grow",
+          }}
+          value={newTitle}
+          disabled={createTaskMutation.isLoading}
+          onChange={(event) => setNewTitle(event.currentTarget.value)}
+        />
+        <ActionIcon
+          loading={createTaskMutation.isLoading}
+          disabled={createTaskMutation.isLoading}
+          onClick={async () => {
+            if (newTitle.length === 0) return;
+            await createTaskMutation.mutateAsync(
+              {
+                title: newTitle,
+                date: startOfDay(new Date(0)),
+              },
+              {
+                onSuccess: async () => {
+                  await utils.kanban.tasks.refetch();
+                  setNewTitle("");
+                },
+              }
+            );
+          }}
+        >
+          <IconCheck color={"green"} />
+        </ActionIcon>
+      </div>
+      <div className="BACKLOG_LIST flex flex-col overflow-y-auto">
+        <div className="flex grow flex-col space-y-2 p-2">
+          <SortableContext
+            items={props.tasksQueryData?.backlog ?? []}
+            id={"backlog"}
+            strategy={verticalListSortingStrategy}
+          >
+            {props.tasksQueryData?.backlog.map((task) => (
+              <Sortable id={task.id} key={task.id} data={task}>
+                <TaskItem key={task.id} task={task} />
+              </Sortable>
+            ))}
+
+            {props.tasksQueryData?.backlog.length === 0 && (
+              <Sortable id={"backlog"} data={{}}>
+                {" "}
+              </Sortable>
+            )}
+          </SortableContext>
+        </div>
+      </div>
+    </div>
+  );
+};
