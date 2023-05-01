23b0d2ffc397fe517b7c46becd16986db060a8fa
Luke Davis <dev@rlukedavis.com>
2023-04-30 22:38:41 -0500

    timer

--- src/components/task/TaskCard.tsx
+++ src/components/task/TaskCard.tsx
@@ -1,16 +1,71 @@
 import { FC, useState } from "react";
 import { ActionIcon, Card, Group, Stack, Text } from "@mantine/core";
 import { useDraggable } from "@dnd-kit/core";
-import { DRAGABLES } from "~/pages";
+import { classNames, DRAGABLES } from "~/pages";
 import { IconCalendar, IconCircleCheck, IconClock } from "@tabler/icons-react";
 import { Timer } from "~/components/task/Timer";
 import { DatePicker } from "@mantine/dates";
 import { api } from "~/utils/api";
 import { TaskData } from "~/server/api/root";
+import { intervalToDuration, isSameDay } from "date-fns";
 
 export const TaskCard: FC<{
   task: TaskData;
-}> = ({ task }) => {
+  dateRange: {
+    startAt: Date;
+    endAt: Date;
+  };
+}> = ({ task, dateRange }) => {
+  const utils = api.useContext();
+  const createTimeEntry = api.task.logTime.useMutation();
+  const completeTask = api.task.toggleCompleted.useMutation({
+    onMutate: async ({ taskId, completed }) => {
+      // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
+      await utils.kanban.tasks.cancel();
+
+      // Snapshot the previous value
+      const previous = utils.kanban.tasks.getData(dateRange);
+      console.info(`previous: ${previous?.tasksByDate.length}`);
+
+      // Optimistically update to the new value
+      utils.kanban.tasks.setData(dateRange, (td) => {
+        if (!td) return td;
+        return {
+          ...td,
+          tasksByDate: td.tasksByDate.map((tbd) => {
+            if (!isSameDay(tbd.date, task.date)) return tbd;
+            return {
+              ...tbd,
+              tasks: tbd.tasks.map((t) => {
+                if (t.id !== taskId) return t;
+                return {
+                  ...t,
+                  completed: completed,
+                };
+              }),
+            };
+          }),
+          backlog: td.backlog.map((t) => {
+            if (t.id !== taskId) return t;
+            return {
+              ...t,
+              completed: completed,
+            };
+          }),
+        };
+      });
+
+      // Return a context object with the snapshotted value
+      return { previous };
+    },
+    onError: (err, variables, context) => {
+      utils.kanban.tasks.setData(dateRange, context?.previous);
+    },
+    onSettled: async () => {
+      await utils.kanban.tasks.invalidate(dateRange);
+      console.info("toggled completed task and invalidated");
+    },
+  });
   const { setNodeRef, attributes, listeners } = useDraggable({
     id: task.id,
     data: {
@@ -18,14 +73,25 @@ export const TaskCard: FC<{
       task: task,
     },
   });
-  const [timerOpen, setTimerOpen] = useState<boolean>(true);
   const [datePickerOpen, setDatePickerOpen] = useState<boolean>(false);
   const [datePickerDate, setDatePickerDate] = useState<Date | null>(task.date);
-  const createTimeEntry = api.task.logTime.useMutation();
-  const utils = api.useContext();
+  const [timerOpen, setTimerOpen] = useState<boolean>(false);
   const totalTimeEntrySeconds = task.timeEntries.reduce(
     (acc, timeEntry) => acc + timeEntry.seconds,
     0
+  );
+
+  const totalTimeInHoursAndMinutes = intervalToDuration({
+    start: new Date(0),
+    end: new Date(totalTimeEntrySeconds * 1000),
+  });
+  // format as hh:mm (with leading zeros)
+  const actualTime = getHoursMinutes(totalTimeInHoursAndMinutes);
+  const expectedTime = getHoursMinutes(
+    intervalToDuration({
+      start: new Date(0),
+      end: new Date(0),
+    })
   );
 
   return (
@@ -40,8 +106,15 @@ export const TaskCard: FC<{
     >
       <Card.Section p={8}>
         <Stack spacing={2}>
-          <Group>
-            <Text weight={600}>{task.title}</Text>
+          <Group position={"apart"}>
+            <Text weight={600} pr={totalTimeEntrySeconds > 0 ? 10 : 0}>
+              {task.title}
+            </Text>
+            {totalTimeEntrySeconds > 0 ? (
+              <span className="absolute right-2 top-2 inline-flex items-center rounded-md bg-gray-50 px-1 py-0.5 text-[6pt] font-bold text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                {actualTime} / {expectedTime}
+              </span>
+            ) : null}
           </Group>
           {task.subtasks.length > 0 ? (
             <Group align={"center"}>
@@ -116,3 +189,33 @@ export const TaskCard: FC<{
     </Card>
   );
 };
+export function getHoursMinutes(totalTimeInHoursAndMinutes: Duration) {
+  // if all are zero, return --:--
+  if (
+    totalTimeInHoursAndMinutes.seconds === 0 &&
+    totalTimeInHoursAndMinutes.minutes === 0 &&
+    totalTimeInHoursAndMinutes.hours === 0
+  ) {
+    return "--:--";
+  }
+
+  // if less than 1 minute, return 00:01
+  if (
+    totalTimeInHoursAndMinutes.seconds &&
+    totalTimeInHoursAndMinutes.seconds < 60 &&
+    totalTimeInHoursAndMinutes.minutes === 0 &&
+    totalTimeInHoursAndMinutes.hours === 0
+  ) {
+    return "00:01";
+  }
+
+  return `${
+    !!totalTimeInHoursAndMinutes.hours && totalTimeInHoursAndMinutes.hours > 9
+      ? totalTimeInHoursAndMinutes.hours
+      : `0${totalTimeInHoursAndMinutes.hours}`
+  }:${
+    !!totalTimeInHoursAndMinutes.minutes && totalTimeInHoursAndMinutes.minutes > 9
+      ? totalTimeInHoursAndMinutes.minutes
+      : `0${totalTimeInHoursAndMinutes.minutes}`
+  }`;
+}
